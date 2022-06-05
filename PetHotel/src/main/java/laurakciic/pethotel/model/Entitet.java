5e9d4cce8c92bbc9bab65447366497972c9244b7
Laura Kovačić <laurakciic@gmail.com>
2022-06-05 14:04:29 +0200

    add superclass Entitet and class VlasnikZivotinje, base mapping

---
+++ PetHotel/src/main/java/laurakciic/pethotel/model/Entitet.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laurakciic.pethotel.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author Laura
+ */
+
+// nasljedivat ce ju ostale 
+// nema tablicu 
+
+@MappedSuperclass
+public class Entitet {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+}
